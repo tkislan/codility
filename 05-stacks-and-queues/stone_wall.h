312d2fb6220bbe44c79f3a03289653baa134fd9e
Tomas Kislan <kislan.tomas@gmail.com>
2015-05-16 13:26:22 +0200

    5.4 stone wall finished

--- 05-stacks-and-queues/stone_wall.h
+++ 05-stacks-and-queues/stone_wall.h
@@ -1,3 +1,20 @@
-int solution(vector<int> &H) {
+#include <stack>
 
+int solution(vector<int> &H) {
+  stack<int> s;
+
+  int result = 1;
+
+  s.push(H.front());
+
+  for (size_t i = 1; i < H.size(); ++i) {
+    while (!s.empty() && s.top() > H[i]) s.pop();
+
+    if (s.empty() || s.top() != H[i]) {
+      s.push(H[i]);
+      ++result;
+    }
+  }
+
+  return result;
 }
