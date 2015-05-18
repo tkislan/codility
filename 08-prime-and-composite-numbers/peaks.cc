93db376f7ee6b165595b002bd6e1fbc75b42fcd9
Tomas Kislan <kislan.tomas@gmail.com>
2015-05-18 20:21:43 +0200

    8.3 peaks done

--- 08-prime-and-composite-numbers/peaks.cc
+++ 08-prime-and-composite-numbers/peaks.cc
@@ -10,5 +10,22 @@ int main() {
   vector<int> input { 1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2 };
   CHECK(solution(input), 3);
 
+  vector<int> input2 { 1, 2, 1, 2, 1, 2, 1, 2, 1 };
+  CHECK(solution(input2), 3);
+
+  vector<int> input3 { 1, 1, 1 };
+  CHECK(solution(input3), 0);
+
+  vector<int> input4 { 1, 2, 1 };
+  CHECK(solution(input4), 1);
+
+  vector<int> input5 { 1, 2, 3, 4, 5, 6 };
+  CHECK(solution(input5), 0);
+
+  vector<int> input6;
+  for (size_t i = 0; i < 99999; ++i) input6.push_back(i);
+  input6.push_back(1);
+  CHECK(solution(input6), 1);
+
   return test_ret;
 }
--- 08-prime-and-composite-numbers/peaks.h
+++ 08-prime-and-composite-numbers/peaks.h
@@ -1,8 +1,39 @@
 int solution(vector<int> &A) {
-  vector<int> peaks;
+  vector<size_t> peaks;
 
   for (size_t i = 1; i < A.size() - 1; ++i) {
     if (A[i] > A[i - 1] && A[i] > A[i + 1]) peaks.push_back(i);
+  }
+
+  if (peaks.empty()) return 0;
+
+  for (size_t i = peaks.size(); i >= 1; --i) {
+    if (A.size() % i != 0) continue;
+
+    auto it = peaks.begin();
+    size_t block_end = 0;
+    size_t block_size = A.size() / i;
+
+    bool ok = true;
+    while (ok && block_end < A.size()) {
+      block_end += block_size;
+
+      if (it == peaks.end()) {
+        ok = false;
+        break;
+      }
+
+      //! If there is no peak in a block
+      if (*it >= block_end) {
+        ok = false;
+        break;
+      }
+
+      //! Ignore peaks until the next block
+      while (*it < block_end && it != peaks.end()) ++it;
+    }
+
+    if (ok) return i;
   }
 
   return 0;
